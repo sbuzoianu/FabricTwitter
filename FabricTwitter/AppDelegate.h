eb039e05d5995bc2b940e20d17b67718cc49bc61
sbuzoianu <stefan@buzoianu.ro>
2015-12-05 11:50:04 +0200

    Initial Commit

---
+++ FabricTwitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FabricTwitter
+//
+//  Created by Stefan on 05/12/15.
+//  Copyright © 2015 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FabricTwitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FabricTwitter
+//
+//  Created by Stefan on 05/12/15.
+//  Copyright © 2015 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
